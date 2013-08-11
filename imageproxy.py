41fa8d2f129c774d4cb55b40bff897a22ca0b3c7
Keith Gaughan <k@stereochro.me>
2013-08-11 02:04:03 +0100

    Pass the config into the application.

--- imageproxy.py
+++ imageproxy.py
@@ -68,8 +68,10 @@ def parse_config(conf):
 
 class ImageProxy(object):
 
-    def __init__(self):
+    def __init__(self, sites, types):
         super(ImageProxy, self).__init__()
+        self.sites = sites
+        self.types = types
 
     def __call__(self, environ, start_response):
         start_response('200 Ok', [('Content-Type', 'text/plain')])
@@ -78,5 +80,5 @@ class ImageProxy(object):
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    svr = make_server('localhost', 8080, ImageProxy())
+    svr = make_server('localhost', 8080, ImageProxy({}, {}))
     svr.serve_forever()
